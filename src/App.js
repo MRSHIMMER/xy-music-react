9fa2e29506ea86898751efa8f9a51e0141d28d7f
xiaoyao <1065645460@qq.com>
2022-03-16 20:14:01 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { memo } from "react";
+import AppHeader from "@/components/app-header";
+import AppFooter from "@/components/app-footer";
+import { useRoutes, Link } from "react-router-dom";
 
 export default memo(function App() {
+	let element = useRoutes([
+		{
+			path: "/test",
+			element: <Home />,
+		},
+	]);
 	return (
 		<div>
+			<AppHeader />
 			<h2>App test5</h2>
+			<Link to="test">跳转</Link>
+			{element}
+			<AppFooter />
 		</div>
 	);
 });
+function Home() {
+	return <div>Home</div>;
+}
---
+++ src/components/app-header/index.js
@@ -0,0 +1,9 @@
+import React, { memo } from "react";
+
+export default memo(function AppHeader() {
+	return (
+		<div>
+			<h2>header</h2>
+		</div>
+	);
+});
