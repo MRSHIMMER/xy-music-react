3b18de5b295bbafff334d20498b1cbdf2cee6a80
xiaoyao <1065645460@qq.com>
2022-03-25 10:54:48 +0800

    add-music-playbar

--- src/pages/player/app-player-bar/index.js
+++ src/pages/player/app-player-bar/index.js
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef, useState } from "react";
+import React, { memo, useCallback, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getSizeImage, formatDate, getPlaySong } from "@/utils/format-utils";
@@ -9,6 +9,9 @@ import { PlaybarWrapper, Control, PlayInfo, Operator } from "./style";
 
 const AppPlayerBar = memo(() => {
 	const [currentTime, setCurrentTime] = useState(0);
+	const [progress, setProgress] = useState(0);
+	const [isChanging, setIsChanging] = useState(false);
+	const [isPlaying, setIsPlaying] = useState(false);
 
 	const dispatch = useDispatch();
 	const currentSong = useSelector(selectCurrentSong);
@@ -20,28 +23,59 @@ const AppPlayerBar = memo(() => {
 	useEffect(() => {
 		if (currentSongStatus === "idle") dispatch(fetchPlayer());
 	}, [currentSongStatus, dispatch]);
+	useEffect(() => {
+		audioRef.current.src = getPlaySong(currentSong.id);
+	}, [currentSong]);
 
 	// 赋值技巧
 	const picUrl = (currentSong.al && currentSong.al.picUrl) || "";
 	const singerName = (currentSong.ar && currentSong.ar[0].name) || "未知歌手";
 	const duration = currentSong.dt || 0;
 	const showDuration = formatDate(duration, "mm:ss");
-	const shoeCurrentTime = formatDate(currentTime, "mm:ss");
-	const progress = (currentTime / duration) * 100;
+	const showCurrentTime = formatDate(currentTime, "mm:ss");
 
-	const playMusic = () => {
-		audioRef.current.src = getPlaySong(currentSong.id);
-		audioRef.current.play();
-	};
+	const playMusic = useCallback(() => {
+		isPlaying ? audioRef.current.pause() : audioRef.current.play();
+		setIsPlaying(!isPlaying);
+	}, [isPlaying]);
 
 	const timeUpdate = (e) => {
-		setCurrentTime(e.target.currentTime * 1000);
+		// setCurrentTime(e.target.currentTime * 1000);
+		if (!isChanging) {
+			setProgress((currentTime / duration) * 100);
+			setCurrentTime(e.target.currentTime * 1000);
+		}
 	};
+
+	//当回调函数传到自定义组件时，使用useCallback优化
+	const sliderChange = useCallback(
+		(value) => {
+			setIsChanging(true);
+			const currentTime = (value / 100) * duration;
+			setCurrentTime(currentTime);
+			setProgress(value);
+		},
+		[duration]
+	);
+
+	const sliderAfterChange = useCallback(
+		(value) => {
+			const currentTime = ((value / 100) * duration) / 1000;
+			audioRef.current.currentTime = currentTime;
+			setCurrentTime(currentTime * 1000);
+			setIsChanging(false);
+
+			if (!isPlaying) {
+				playMusic();
+			}
+		},
+		[duration, isPlaying, playMusic]
+	);
 
 	return (
 		<PlaybarWrapper className="sprite_player">
 			<div className="content wrap-v2">
-				<Control>
+				<Control isPlaying={isPlaying}>
 					<button className="sprite_player prev"></button>
 					<button className="sprite_player play" onClick={(e) => playMusic()}></button>
 					<button className="sprite_player next"></button>
@@ -60,9 +94,14 @@ const AppPlayerBar = memo(() => {
 							</a>
 						</div>
 						<div className="progress">
-							<Slider defaultValue={30} value={progress} />
+							<Slider
+								defaultValue={30}
+								value={progress}
+								onChange={sliderChange}
+								onAfterChange={sliderAfterChange}
+							/>
 							<div className="time">
-								<span className="now-time">{shoeCurrentTime}</span>
+								<span className="now-time">{showCurrentTime}</span>
 								<span className="divider"></span>
 								<span className="duration">{showDuration}</span>
 							</div>
