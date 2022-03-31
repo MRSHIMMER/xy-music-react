ca5718b2972b8eaa251b5f5953db20cdda50affc
xiaoyao <1065645460@qq.com>
2022-03-31 16:20:39 +0800

    更改生产环境配置

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,6 @@
 const devBaseURL = "https://netease-cloud-music-api-mrshimmer.vercel.app/";
-const proBaseURL = "https://production.org";
+// const proBaseURL = "https://production.org";
+const proBaseURL = "https://netease-cloud-music-api-mrshimmer.vercel.app/";
 export const BASE_URL =
 	process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;
 
