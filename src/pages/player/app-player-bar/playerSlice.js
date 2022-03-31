820a3bd9d4b831d43521a53c50ab9f661e243578
xiaoyao <1065645460@qq.com>
2022-03-31 11:25:51 +0800

    获取歌词

--- src/pages/player/app-player-bar/playerSlice.js
+++ src/pages/player/app-player-bar/playerSlice.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { getSongDetail } from "@/services/player";
+import { getSongDetail, getLyric } from "@/services/player";
 import { getRandomNumber } from "@/utils/math-utils";
+import { parseLyric } from "@/utils/parse-lyric";
 
 const initialState = {
 	currentSongIndex: 0,
@@ -9,10 +10,12 @@ const initialState = {
 	playList: [],
 	status: "idle",
 	playerSequence: 0, //0 循环 1 随机 2 单曲循环
+	lyricList: [],
+	currentLyricIndex: 0,
 };
 
 export const getSongDetailThunk = (ids) => {
-	return (dispatch, getState) => {
+	return async (dispatch, getState) => {
 		// 1.根据id查找playList中是否已经有了该歌曲
 		const playList = getState().player.playList;
 		const songIndex = playList.findIndex((song) => song.id === ids);
@@ -24,22 +27,25 @@ export const getSongDetailThunk = (ids) => {
 			dispatch(changeCurrentSongIndex(songIndex));
 			song = playList[songIndex];
 			dispatch(changeCurrentSong(song));
+			dispatch(fetchLyric(song.id));
 		} else {
 			// 没有找到歌曲
 			// 请求歌曲数据
-			getSongDetail(ids).then((res) => {
-				song = res.songs && res.songs[0];
-				if (!song) return;
+			const res = await getSongDetail(ids);
+			song = res.songs && res.songs[0];
+			if (!song) return;
 
-				// 1.将最新请求到的歌曲添加到播放列表中
-				const newPlayList = [...playList];
-				newPlayList.push(song);
+			// 1.将最新请求到的歌曲添加到播放列表中
+			const newPlayList = [...playList];
+			newPlayList.push(song);
 
-				// 2.更新redux中的值
-				dispatch(changePlayList(newPlayList));
-				dispatch(changeCurrentSongIndex(newPlayList.length - 1));
-				dispatch(changeCurrentSong(song));
-			});
+			// 2.更新redux中的值
+			dispatch(changePlayList(newPlayList));
+			dispatch(changeCurrentSongIndex(newPlayList.length - 1));
+			dispatch(changeCurrentSong(song));
+
+			// 3. 请求该歌曲的歌词
+			dispatch(fetchLyric(song.id));
 		}
 	};
 };
@@ -69,14 +75,28 @@ export const changeSongThunk = (tag) => {
 		const currentSong = playList[currentSongIndex];
 		dispatch(changeCurrentSong(currentSong));
 		dispatch(changeCurrentSongIndex(currentSongIndex));
+		//请求歌词
+		dispatch(fetchLyric(currentSong.id));
 	};
 };
 
 export const fetchSong = createAsyncThunk("player/fetchSong", async (ids) => {
 	// 这个payload creator回调函数一定要返回promise，改用普通的thunk
+	// 后期再试试是否会有Bug，下面fetchLyric没有返回promise，依然可以正常使用
 	const response = await getSongDetail(ids);
 	return response.songs[0];
 });
+
+export const fetchLyric = createAsyncThunk(
+	"player/fetchLyric",
+	async (id, object) => {
+		const response = await getLyric(id);
+		const lyric = parseLyric(response.lrc.lyric);
+		return lyric;
+		// response.lrc.lyric = parseLyric(response.lrc.lyric);
+		// return response.lrc.lyric;
+	}
+);
 
 const playerSlice = createSlice({
 	name: "player",
@@ -105,6 +125,9 @@ const playerSlice = createSlice({
 		},
 		[fetchSong.rejected]: (state, action) => {
 			state.status = "failed";
+		},
+		[fetchLyric.fulfilled]: (state, action) => {
+			state.lyricList = action.payload;
 		},
 	},
 });
